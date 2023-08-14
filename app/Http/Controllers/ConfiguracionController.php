87d3d6e690875475b7334bc320b9fae2d4e72ffc
Edgar Alvarez OKC <programador2@okcomputer.com.pe>
2023-08-14 16:58:08 -0500

    editar datos usuario

--- app/Http/Controllers/ConfiguracionController.php
+++ app/Http/Controllers/ConfiguracionController.php
@@ -895,6 +895,7 @@ class ConfiguracionController extends Controller{
         $sis_usua->estado           = 1;
         $sis_usua->fecha_registro   = date('Y-m-d H:i:s');
         $sis_usua->nombre_corto     = $request->nombre_corto;
+        $sis_usua->nombre_largo     = $request->nombre_largo;
         $sis_usua->codvend_softlink = $request->codvent_softlink;
         $sis_usua->email            = $request->email;
         $sis_usua->save();

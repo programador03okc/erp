0834a73812adf4cfa76343d8a0302f507e07cac9
raul <raulsalinasvaldivia@gmail.com>
2023-08-25 16:23:45 -0500

    migrar orden con codigo de oc agil en obs

--- app/Http/Controllers/Migraciones/MigrateOrdenSoftLinkController.php
+++ app/Http/Controllers/Migraciones/MigrateOrdenSoftLinkController.php
@@ -293,7 +293,7 @@ class MigrateOrdenSoftLinkController extends Controller
                                         'mon_bruto' => $oc->total_precio,
                                         'mon_impto1' => $mon_impto,
                                         'mon_total' => ($oc->total_precio + $mon_impto),
-                                        'txt_observa' => ($oc->observacion !== null ? $oc->observacion : ''),
+                                        'txt_observa' => ($oc->codigo).' / '.($oc->observacion !== null ? $oc->observacion : ''),
                                         'cod_user' => $oc->codvend_softlink,
                                         'tip_cambio' => $tp_cambio->cambio3, //tipo cambio venta
                                         'ndocu1' => ($oc->plazo_entrega !== null ? $oc->plazo_entrega . ' DIAS' : ''),

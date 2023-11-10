2dfef4fb84a1342c501e13247bf312834366fa89
raul <programador03@okcomputer.com.pe>
2023-11-10 14:19:48 -0500

    exportar lista de reservas en excel

--- public/js/almacen/reservas/reservasAlmacen.js
+++ public/js/almacen/reservas/reservasAlmacen.js
@@ -1,9 +1,59 @@
 let $tableReservas;
 
+function newexportaction(e, dt, button, config) {
+    var self = this;
+    var oldStart = dt.settings()[0]._iDisplayStart;
+    dt.one('preXhr', function (e, s, data) {
+        // Just this once, load all data from the server...
+        data.start = 0;
+        data.length = 2147483647;
+        dt.one('preDraw', function (e, settings) {
+            // Call the original action function
+            if (button[0].className.indexOf('buttons-copy') >= 0) {
+                $.fn.dataTable.ext.buttons.copyHtml5.action.call(self, e, dt, button, config);
+            } else if (button[0].className.indexOf('buttons-excel') >= 0) {
+                $.fn.dataTable.ext.buttons.excelHtml5.available(dt, config) ?
+                    $.fn.dataTable.ext.buttons.excelHtml5.action.call(self, e, dt, button, config) :
+                    $.fn.dataTable.ext.buttons.excelFlash.action.call(self, e, dt, button, config);
+            } else if (button[0].className.indexOf('buttons-csv') >= 0) {
+                $.fn.dataTable.ext.buttons.csvHtml5.available(dt, config) ?
+                    $.fn.dataTable.ext.buttons.csvHtml5.action.call(self, e, dt, button, config) :
+                    $.fn.dataTable.ext.buttons.csvFlash.action.call(self, e, dt, button, config);
+            } else if (button[0].className.indexOf('buttons-pdf') >= 0) {
+                $.fn.dataTable.ext.buttons.pdfHtml5.available(dt, config) ?
+                    $.fn.dataTable.ext.buttons.pdfHtml5.action.call(self, e, dt, button, config) :
+                    $.fn.dataTable.ext.buttons.pdfFlash.action.call(self, e, dt, button, config);
+            } else if (button[0].className.indexOf('buttons-print') >= 0) {
+                $.fn.dataTable.ext.buttons.print.action(e, dt, button, config);
+            }
+            dt.one('preXhr', function (e, s, data) {
+                // DataTables thinks the first item displayed is index 0, but we're not drawing that.
+                // Set the property to what it was before exporting.
+                settings._iDisplayStart = oldStart;
+                data.start = oldStart;
+            });
+            // Reload the grid with the original page. Otherwise, API functions like table.cell(this) don't work properly.
+            setTimeout(dt.ajax.reload, 0);
+            // Prevent rendering of the full data to the DOM
+            return false;
+        });
+    });
+    // Requery the server with the new one-time export settings
+    dt.ajax.reload();
+}
+
 function listarReservasAlmacen(id_usuario) {
-    console.log('list');
+    // console.log('list');
     var vardataTables = funcDatatables();
-    let botones = [];
+    let botones = [
+        {
+            "extend": 'excel',
+            "text": '<i class="far fa-file-excel"></i> Exportar',
+            "titleAttr": 'Excel',
+            "action": newexportaction
+
+        }
+    ];
 
     $("#reservasAlmacen").on('search.dt', function () {
         $('#reservasAlmacen_filter input').prop('disabled', true);
@@ -28,8 +78,10 @@ function listarReservasAlmacen(id_usuario) {
         buttons: botones,
         language: vardataTables[0],
         // destroy: true,
-        pageLength: 10,
+        // pageLength: 10,
         serverSide: true,
+        lengthMenu: [[10, 25, 50, -1], [10, 25, 50, "All"]],
+
         initComplete: function (settings, json) {
             const $filter = $("#reservasAlmacen_filter");
             const $input = $filter.find("input");

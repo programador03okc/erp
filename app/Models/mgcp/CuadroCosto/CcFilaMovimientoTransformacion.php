7e14d023b518130c17c33691420b4a7c4b0b4e7d
programador03 <programador03@okcomputer.com.pe>
2024-06-19 15:35:46 -0500

    update CcFilaMovimientoTransformacion

--- app/Models/mgcp/CuadroCosto/CcFilaMovimientoTransformacion.php
+++ app/Models/mgcp/CuadroCosto/CcFilaMovimientoTransformacion.php
@@ -4,13 +4,16 @@ namespace App\Models\mgcp\CuadroCosto;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CcFilaMovimientoTransformacion extends Model
 {
-    // use HasFactory;
+    use HasFactory, SoftDeletes;
+
     protected $table = 'mgcp_cuadro_costos.cc_fila_movimientos_transformacion';
     public $timestamps = false;
-
+    protected $hidden = ['created_at', 'updated_at', 'deleted_at'];
+    
     public function setSaleAttribute($value)
     {
         $this->attributes['sale'] = mb_strtoupper($value);
